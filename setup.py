0b28c65cd9d22a6538058656a2fca9baae8202e1
lidar-guy <alexander.lesnick@gm.com>
2025-01-26 13:10:15 -0500

    test 123

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = "0.0.0"
 REPO_NAME = "End_to_end_MLOPS_project"
 AUTHOR_USER_NAME = "lidar-guy"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "alex.j.lesnick@gmail.com"
+AUTHOR_EMAIL = "alexander.lesnick@gm.com"
 
 
 setuptools.setup(
